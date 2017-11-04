b5fd6adda2bdff76e52e05523e9e4b27e184a16e
Brian Bycynski <high5bri@hotmail.com>
2017-11-04 11:56:15 -0400

    added redux

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
-
+import AppRouter from '../routers/AppRouter'
 class App extends Component {
   render () {
-    return <div>
-      <h1>Hello, World!</h1>
-    </div>
+    return (
+      <div>
+        <AppRouter />
+      </div>
+    )
   }
 }
 
---
+++ src/components/EditExpensePage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const EditExpensePage = (props) => {
+  return (
+    <div>
+    Edit Expense Page {props.match.params.id}
+    </div>
+  )
+}
+export default EditExpensePage
