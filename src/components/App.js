6292fbc45b45bbffd65ac3a13f5b66c4ce1d6065
Brian Bycynski <high5bri@hotmail.com>
2017-11-05 10:32:26 -0500

    still hooking up redux

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import AppRouter from '../routers/AppRouter'
+
 class App extends Component {
   render () {
     return (
--- src/index.js
+++ src/index.js
@@ -2,10 +2,23 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { AppContainer } from 'react-hot-loader'
 import Redbox from 'redbox-react'
-
+import configureStore from './store/configureStore'
+import { addExpense } from './actions/expenses'
+import { setTextFilter } from './actions/filters'
+import getVisibleExpenses from './selectors/expenses'
 import App from './components/App'
-
 import './styles/screen.scss'
+
+const store = configureStore()
+
+store.dispatch(addExpense({ description: 'Water Bill Bozo' }))
+store.dispatch(addExpense({ description: 'Gas Bill Bozo' }))
+store.dispatch(setTextFilter('Water'))
+
+const state = store.getState()
+const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
+
+console.log(visibleExpenses)
 
 const root = document.getElementById('root')
 
